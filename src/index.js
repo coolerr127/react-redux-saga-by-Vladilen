683581594ac1fff107c52bc8da2eabcb5afc9422
Anton Shimanovich <ashimanovich127@gmail.com>
2021-07-28 14:47:05 +0300

    added redux, sync posts, form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { render } from 'react-dom'
+import thunk from 'redux-thunk'
+import reportWebVitals from './reportWebVitals'
+import { compose, createStore, applyMiddleware } from 'redux'
+import App from './App'
+import { rootReducer } from './redux/rootReducer'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(
+	rootReducer,
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+)
+
+const app = (
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+)
+
+render(app, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ src/redux/types.js
@@ -0,0 +1,3 @@
+export const CREATE_POST = 'POST/CREATE_POST'
+
+export const FETCH_POST = 'POST/FETCH_POST'
